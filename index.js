1146a591b29737edad2812080c0b105713ffedea
Danya <shemetg@gmail.com>
2020-08-11 14:28:54 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var product = "Socks";
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: '1212',
+        description: 'A pair of warm, fuzzy socks.',
+        image: "./assets/img/vmSocks-green.jpg"
+    }
+})
